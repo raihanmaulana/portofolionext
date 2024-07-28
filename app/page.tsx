5dbaf206c4be35aeecbfd3b5f44a92985ecdc537
raihanmaulana <raihanmaulana09@gmail.com>
2024-07-28 20:46:15 +0700

    portofolio

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import Profile from "@/components/component/profile";
 import Separator from "@/components/component/separator";
 import ThemeToggle from "@/components/component/theme-toggle";
 
-import Macfolio from "@/public/macfolio.webp";
+import SIKPB from "@/public/SIKPB.png";
+import Blank from "@/public/blank.jpg";
 import Nextbase from "@/public/nextbase.webp";
 import { ArrowUp } from "lucide-react";
 import Image from "next/image";
@@ -14,24 +15,28 @@ export default function Home() {
       <div className="flex flex-col max-w-xl w-full h-auto grow gap-y-8 md:gap-y-12">
         <Separator
           leftItem="based in 🇮🇩"
-          rightItem="msaf.tech"
+          rightItem="raihanmaulana"
           animated
-          link="msaf.tech"
+          link=""
           textName="group-hover:text-primary"
           borderName="bg-primary"
         />
         {/* Bio */}
         <div className="flex flex-col gap-y-4">
           <Profile />
-          <p className="text-muted-foreground text-xs md:text-sm">
-            I'm a software engineer based in{" "}
-            <span className="text-foreground font-medium">Indonesia</span>. I'm
-            passionate about creating software that is simple, delightful to
-            use, and easy to maintain. Currently{" "}
-            <span className="text-foreground font-medium italic">
-              tinkering
-            </span>{" "}
-            with my final assignment and side-projects.
+          <p
+            className="text-muted-foreground text-xs md:text-sm"
+            style={{ textAlign: "justify" }}
+          >
+            As a dedicated software engineer based in{" "}
+            <span className="text-foreground font-medium">Indonesia</span>, I am
+            passionate about crafting software solutions that are not only
+            functional but also intuitive and enjoyable to use. My focus is on
+            creating applications that are simple, maintainable, and delight
+            users with their seamless experiences. Currently, I am channeling my
+            energy into various side-projects, continually exploring new
+            technologies and methodologies to refine my skills and deliver
+            exceptional results.
           </p>
         </div>
         {/* Links */}
@@ -42,31 +47,22 @@ export default function Home() {
             className="mb-2"
           />
           <Separator
-            leftItem="compactfolio"
-            rightItem="get this template"
-            variant="primary"
-            animated
-            textName="group-hover:text-indigo-400"
-            borderName="bg-indigo-500"
-            link="https://compact.msaf.tech"
-          />
-          <Separator
-            leftItem="macfolio"
-            rightItem="get this template"
+            leftItem="profile"
+            rightItem="linkedin"
             variant="primary"
             animated
             textName="group-hover:text-primary"
             borderName="bg-primary"
-            link="https://mac.msaf.tech"
+            link="https://www.linkedin.com/in/raihanmaulana/"
           />
           <Separator
-            leftItem="@msafdev"
+            leftItem="@raihanmaulana"
             rightItem="github.com"
             variant="primary"
             animated
             textName="group-hover:text-fuchsia-400"
             borderName="bg-fuchsia-400"
-            link="https://github.com/msafdev"
+            link="https://github.com/raihanmaulana"
           />
           <Separator
             leftItem="resume.pdf"
@@ -75,7 +71,7 @@ export default function Home() {
             animated
             textName="group-hover:text-green-400"
             borderName="bg-green-400"
-            link="https://drive.google.com/file/d/1mCvY4uWDmKjsaQEBkDzQZC4bc-AkZHPK/view?usp=sharing"
+            link="https://drive.google.com/file/d/1jkVaFygszqF77HEC6jEJ2pfnr4ojvhdV/view?usp=sharing"
           />
         </div>
         {/* Projects */}
@@ -84,20 +80,36 @@ export default function Home() {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-y-3 md:gap-x-4">
             <div className="flex flex-col gap-y-1.5">
               <Image
-                src={Macfolio}
+                src={SIKPB}
                 alt="macfolio"
                 className="border-2 rounded-2xl bg-accent p-4 object-scale-down aspect-[4/3]"
               />
               <Link
-                href={"https://mac.msaf.tech"}
+                href={"https://yasawaskitha.site"}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-foreground font-medium text-center text-xs md:text-sm"
               >
-                Macfolio <ArrowUp size={14} className="inline rotate-45" />
+                SIPKB <ArrowUp size={14} className="inline rotate-45" />
               </Link>
             </div>
             <div className="flex flex-col gap-y-1.5">
+              <Image
+                src={Blank}
+                alt="macfolio"
+                className="border-2 rounded-2xl bg-accent p-4 object-scale-down aspect-[4/3]"
+              />
+              <Link
+                href={"#"}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-foreground font-medium text-center text-xs md:text-sm"
+              >
+                Coming Soon Projects{" "}
+                <ArrowUp size={14} className="inline rotate-45" />
+              </Link>
+            </div>
+            {/* <div className="flex flex-col gap-y-1.5">
               <Image
                 src={Nextbase}
                 alt="nextbase"
@@ -111,47 +123,47 @@ export default function Home() {
               >
                 Nextbase <ArrowUp size={14} className="inline rotate-45" />
               </Link>
-            </div>
+            </div> */}
           </div>
         </div>
         {/* Stack */}
         <div className="flex flex-col gap-y-1.5 w-full">
           <Separator leftItem="03" rightItem="stack" className="mb-2" />
-          <Separator leftItem="figma" rightItem="design" variant="primary" />
           <Separator
-            leftItem="next.js"
+            leftItem="adobe photoshop"
+            rightItem="design"
+            variant="primary"
+          />
+          <Separator
+            leftItem="next.js,react.js"
             rightItem="frontend"
             variant="primary"
           />
-          <Separator leftItem="nest" rightItem="backend" variant="primary" />
+          <Separator leftItem="laravel" rightItem="backend" variant="primary" />
           <Separator leftItem="typescript" rightItem="lang" variant="primary" />
-          <Separator leftItem="hygraph" rightItem="cms" variant="primary" />
-          <Separator
-            leftItem="supabase"
-            rightItem="database"
-            variant="primary"
-          />
+          {/* <Separator leftItem="hygraph" rightItem="cms" variant="primary" /> */}
+          <Separator leftItem="mysql" rightItem="database" variant="primary" />
         </div>
         {/* Todos */}
         <div className="flex flex-col gap-y-1.5 w-full">
           <Separator leftItem="to do" rightItem="2024" className="mb-2" />
           <Separator
-            leftItem="finish tomodoro"
+            leftItem="more projects"
             rightItem="in progress"
             variant="primary"
           />
           <Separator
-            leftItem="reach 100 stars"
+            leftItem="focus learning more about fullstack"
             rightItem="in progress"
             variant="primary"
           />
-          <Separator
+          {/* <Separator
             leftItem="ship 10 stuff"
             rightItem="in progress"
             variant="primary"
-          />
-          <Separator leftItem="launch compactfolio" rightItem="done" />
-          <Separator leftItem="launch macfolio" rightItem="done" />
+          /> */}
+          <Separator leftItem="finish my college" rightItem="done" />
+          <Separator leftItem="get my first salary" rightItem="done" />
         </div>
         {/* Footer */}
         <div className="flex flex-col gap-y-1.5 w-full">
@@ -166,13 +178,7 @@ export default function Home() {
               borderName="bg-primary"
             />
           </ThemeToggle>
-          <Separator
-            leftItem="29 may, 2024"
-            rightItem="latest update"
-            variant="primary"
-          />
-          <Separator leftItem="v1.02" rightItem="version" variant="primary" />
-          <Separator leftItem="© 2024 msafdev" rightItem="with love" />
+          <Separator leftItem="© 2024 raihanmaulana" rightItem="with love" />
         </div>
       </div>
     </main>
--- components/component/profile.tsx
+++ components/component/profile.tsx
@@ -13,17 +13,19 @@ const Profile = () => {
         <Avatar className="h-10 w-10 md:h-12 md:w-12 rounded-sm">
           <AvatarImage
             className="rounded-sm"
-            src="https://github.com/msafdev.png"
-            alt="@msafdev"
+            src="https://github.com/raihanmaulana.png"
+            alt="@raihanmaulana"
           />
           <AvatarFallback className="rounded-sm">CN</AvatarFallback>
         </Avatar>
       </Squircle>
       <div className="flex flex-col gap-y-1">
         <h2 className="text-base md:text-lg font-semibold text-foreground leading-none md:leading-none">
-          Salman Alfarisi
+          Raihan Maulana
         </h2>
-        <p className="text-xs md:text-sm text-muted-foreground leading-none md:leading-none">@msafdev</p>
+        <p className="text-xs md:text-sm text-muted-foreground leading-none md:leading-none">
+          @raihanmaulana
+        </p>
       </div>
     </div>
   );
